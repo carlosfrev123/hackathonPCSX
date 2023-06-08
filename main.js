29a01ac0ed7096113d176edbfddf2ec747c16583
Carlos Revilla <carlosrevilla@Gilbert.local>
2023-06-07 20:02:51 -0400

    hack commit

---
+++ main.js
@@ -0,0 +1,27 @@
+chrome.runtime.onInstalled.addListener(('click'), function(){
+    console.log("testing");
+})
+
+
+
+
+// const addBox = () => {
+    
+// }
+
+
+
+// const create_overlay = () => {
+    
+// }
+
+
+// //  mouse click 
+// const toggle_overlay = () => {
+//     let element = document.getElementById('box');
+//     //if showing, unshow. if unshowing, show
+//     //if the display is not on, turn the display on
+//     element.style.display = none;
+    
+// }
+    
